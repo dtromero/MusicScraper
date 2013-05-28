cf03e373bd7c622b319efe4dc2152ae47554dc0c
Daniel Romero <dtromero@gmail.com>
2013-05-28 15:57:41 -0400

    second commit - working version of BeautifulSoup mp3 link extractor

--- mp3scraper.py
+++ mp3scraper.py
@@ -3,13 +3,17 @@ import urllib2
 import re
 from bs4 import BeautifulSoup
 
-url = "http://www.aquariumdrunkard.com"
-page = urllib2.urlopen(url)
+url = "http://www.example.com/"
+page = urllib2.urlopen(url).read()
 soup = BeautifulSoup(page)
 
-links = soup.find_all('a') #, href=re.compile('$mp3'))
+#links = soup.find_all('a') #, href=re.compile('$mp3'))
+#links = soup.find_all('a', href=re.compile('(?i)(mp3)$'))
+#links = soup.find_all('a', href=re.compile('(mp3|mp3 )$'))
+links = soup.find_all('a', href=re.compile(r'mp3'))
 
 for link in links:
-	#linkName = link.contents[0]
+	linkName = link.contents[0]
 	fullLink = link.get('href')
 	print fullLink
+	print linkName
