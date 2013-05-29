4022ccfafb1e98a45531f8a177a92c55e1b39591
Daniel Romero <dtromero@gmail.com>
2013-05-28 21:28:31 -0400

    Version 3: Functioning urllib2 and BeautifulSoup mp3 downloader

--- mp3scraper.py
+++ mp3scraper.py
@@ -1,19 +1,28 @@
-
+import os
 import urllib2
 import re
 from bs4 import BeautifulSoup
 
 url = "http://www.example.com/"
+urltag = "example"
+filepath = "/home/user/Music/"
+
+musicfolder = os.path.join(filepath,urltag)
 page = urllib2.urlopen(url).read()
 soup = BeautifulSoup(page)
 
-#links = soup.find_all('a') #, href=re.compile('$mp3'))
-#links = soup.find_all('a', href=re.compile('(?i)(mp3)$'))
-#links = soup.find_all('a', href=re.compile('(mp3|mp3 )$'))
 links = soup.find_all('a', href=re.compile(r'mp3'))
 
+if not os.path.exists(musicfolder):
+	os.makedirs(musicfolder)
+
 for link in links:
-	linkName = link.contents[0]
+	linkString = link.contents[0]
+	linkName = linkString.replace("/","").replace("\\","") + ".mp3"
 	fullLink = link.get('href')
-	print fullLink
-	print linkName
+	print "Downloading: " + linkName + " | To: " + filepath + urltag
+	
+	f = urllib2.urlopen(fullLink)
+
+	with open(os.path.join(musicfolder,linkName), "wq") as local_file:
+		local_file.write(f.read())
